0e90a2c5c18bbf328a75445e234ce662ebc7afb1
Volkova Maria <volkova.maria.d@gmail.com>
2017-11-18 10:01:43 +0300

    back

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM openjdk:alpine
 EXPOSE 8080
 RUN apk add --no-cache git
 RUN apk add --no-cache maven
-RUN git clone https://github.com/stakenschneider/validationService
-WORKDIR /validationService
+RUN git clone https://github.com/stakenschneider/validationservice
+WORKDIR /validationservice
 RUN mvn clean install -e
 CMD mvn exec:java -e
--- src/main/java/pack/Resource.java
+++ src/main/java/pack/Resource.java
@@ -3,31 +3,57 @@ package pack;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import com.google.gson.*;
+import javax.ws.rs.Consumes;
+
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.stream.Collectors;
 
-@Path("")
 
+@Path("")
 public class Resource {
 
     @POST
-    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
 
-    public Response checkFile(InputStream stream){
+    public Response checkFile(@FormDataParam("file") InputStream stream,
+                              @FormDataParam("file") FormDataContentDisposition fileDetail) throws Exception {
+
         final String json = new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining());
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Object result;
-        try {
-            result = gson.fromJson(json, Object.class);
-        } catch (JsonSyntaxException e) {
-            return Response.status(200).entity(e.getCause().getMessage()).build();
-        }
-        return Response.status(200).entity(gson.toJson(result)).build();
+        Map<String, String> result;
+
+//        try {
+//            result = gson.fromJson(json, new TypeToken<Map<String, String>>() {
+//            }.getType());
+//        } catch (JsonSyntaxException e) {
+//            return Response.status(200).entity(gson.toJson(makeError(e, fileDetail))).build();
+//        }
+
+        return Response.status(200).entity(gson.toJson(json)).build();
+    }
+
+    private Map<String, String> makeError(JsonSyntaxException e, FormDataContentDisposition fileDetail) {
+        String messageDetail = e.getCause().getMessage();
+        Map<String, String> error = new HashMap<>();
+        String[] arrayMessage = messageDetail.split("at", 2);
+        error.put("resource", fileDetail.getFileName());
+        error.put("errorPlace", arrayMessage[1]);
+        error.put("errorMessage", arrayMessage[0]);
+        return error;
     }
 }
